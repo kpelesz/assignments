9686007a99c173400a5c42e734a7b1483b8f2205
Katya Pelesz <kpelesz@gmail.com>
2017-05-31 14:54:05 -0400

    Create leapyear.sh

---
+++ leapyear.sh
@@ -0,0 +1,21 @@
+#Name: Katherine Pelesz
+#Course: OPS435
+#Section: A
+
+#LAB
+#Tells you if you were born on a leapyear
+
+#!/bin/bash
+
+read -p "Please enter a year in the format of (yyyy): " year
+days=$(cal $year |grep -E "^[ 0-9][0-9]| [ 0-9][0-9]$" |wc -w)
+
+if ((($days % 2) == 0 ))
+then
+       echo "This year is a leap year "
+else
+        echo "This year is not a leap year"
+        (exit 9)
+fi
+
+echo "Exit status: " $?
